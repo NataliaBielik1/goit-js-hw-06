59e682ef199e67f7faed173dc5ec0212df5cd985
NataliaBielik1 <116969531+NataliaBielik1@users.noreply.github.com>
2023-03-30 18:09:26 +0300

    Task 2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ul = document.getElementById('ingredients');
+const lis = ingredients.map(i => {
+  const li = document.createElement('li');
+  li.innerText = i;
+  li.classList.add('item');
+  return li;
+});
+
+ul.append(...lis);
