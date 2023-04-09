16ed6d9b026821242fd188d810ebe0925da73750
NataliaBielik1 <116969531+NataliaBielik1@users.noreply.github.com>
2023-04-09 12:26:08 +0300

    task 8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,23 @@
+const formEl = document.querySelector(".login-form");
 
+formEl.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (!email.value || !password.value) {
+    alert(`Всі поля повинні бути заповнені!`);
+    return;
+  }
+
+  const dataForm = {
+    [email.name]: email.value,
+    [password.name]: password.value,
+  };
+
+  console.log(dataForm);
+  event.currentTarget.reset();
+}
