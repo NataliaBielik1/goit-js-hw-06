ef682e79653cbe0002124ae8973ed9c677d91994
NataliaBielik1 <116969531+NataliaBielik1@users.noreply.github.com>
2023-04-09 12:41:22 +0300

    task 9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,3 +3,16 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const buttonEl = document.querySelector(".change-color");
+
+buttonEl.addEventListener("click", handlerButton);
+
+const spanColor = document.querySelector(".color");
+
+function handlerButton() {
+  const resultColor = getRandomHexColor();
+
+  spanColor.textContent = resultColor;
+
+  document.body.style.backgroundColor = resultColor;
+}
