11b36419d966c28119b1247d1895b34705d51ac8
NataliaBielik1 <116969531+NataliaBielik1@users.noreply.github.com>
2023-04-09 12:04:03 +0300

    task 6

---
+++ src/js/task-06.js
@@ -0,0 +1,15 @@
+const inputEl = document.querySelector("#validation-input");
+inputEl.addEventListener("blur", onInputBlur);
+
+function onInputBlur(event) {
+  if (
+    event.currentTarget.value.length ===
+    Number(event.currentTarget.dataset.length)
+  ) {
+    event.currentTarget.classList.add("valid");
+    event.currentTarget.classList.remove("invalid");
+  } else {
+    event.currentTarget.classList.add("invalid");
+    event.currentTarget.classList.remove("valid");
+  }
+}
